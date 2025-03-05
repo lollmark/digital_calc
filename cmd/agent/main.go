ba86fb68b5cf1eeeb67f150a54a12718b3f421d7
lollmark <markello2007@gmail.com>
2025-03-05 23:10:55 +0300

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,42 +1,40 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"sync"
-	"time"
-	"digitalcalc/internal/agent"
-	"digitalcalc/internal/calculator"
-	"go.uber.org/zap"
+    "log"
+    "os"
+    "strconv"
+    "sync"
+    "time"
+
+    "digitalcalc/internal/agent"
+    "go.uber.org/zap"
 )
 
 func main() {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatal("Failed to initialize logger", err)
-	}
-	defer logger.Sync()
+    logger, err := zap.NewProduction()
+    if err != nil {
+        log.Fatal("Failed to initialize logger", err)
+    }
+    defer logger.Sync()
 
-	computingPower, err := strconv.Atoi(os.Getenv("COMPUTING_POWER"))
-	if err != nil || computingPower <= 0 {
-		computingPower = 2 // По умолчанию 2 горутины
-	}
+    computingPower, err := strconv.Atoi(os.Getenv("COMPUTING_POWER"))
+    if err != nil || computingPower <= 0 {
+        computingPower = 2 
+        logger.Warn("COMPUTING_POWER not set or invalid, using default value", zap.Int("default", computingPower))
+    }
 
-	var wg sync.WaitGroup
-	for i := 0; i < computingPower; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			logger.Info("Agent worker started", zap.Int("id", id))
-			for {
-				agent.Work(logger)
-				time.Sleep(1 * time.Second) // Задержка между запросами
-			}
-		}(i)
-	}
-	wg.Wait()
+    var wg sync.WaitGroup
+    for i := 0; i < computingPower; i++ {
+        wg.Add(1)
+        go func(id int) {
+            defer wg.Done()
+            logger.Info("Agent worker started", zap.Int("id", id))
+            for {
+                agent.Work(logger)
+                time.Sleep(1 * time.Second) 
+            }
+        }(i)
+    }
+    wg.Wait()
 }
