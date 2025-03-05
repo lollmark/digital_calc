86bf73e26882c62dc5fac82eb8d072bc73070f6d
lollmark <markello2007@gmail.com>
2025-03-05 23:08:00 +0300

    Update status.go

--- internal/models/status.go
+++ internal/models/status.go
@@ -1,12 +1,10 @@
 package models
 
-// Status определяет текущий статус выражения
 type Status string
 
-// Константы для возможных статусов выражения
 const (
-	StatusPending    Status = "PENDING"    // Ожидает выполнения
-	StatusProcessing Status = "PROCESSING" // В процессе выполнения
-	StatusCompleted  Status = "COMPLETED"  // Вычисление завершено
-	StatusError      Status = "ERROR"      // Ошибка при вычислении
+	StatusPending    Status = "PENDING"    
+	StatusProcessing Status = "PROCESSING"
+	StatusCompleted  Status = "COMPLETED"  
+	StatusError      Status = "ERROR"      
 )
