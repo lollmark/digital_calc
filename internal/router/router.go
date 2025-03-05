0261c13c8b2b14b292770c29c4fd4d31c7c6eee5
lollmark <markello2007@gmail.com>
2025-03-05 23:04:06 +0300

    Update router.go

--- internal/router/router.go
+++ internal/router/router.go
@@ -8,25 +8,19 @@ import (
 	"go.uber.org/zap"
 )
 
-// NewRouter создает новый маршрутизатор с применением middleware и обработчиков.
 func NewRouter(logger *zap.Logger) http.Handler {
-	// Создаем сервер оркестратора
 	server := orchestrator.NewServer(logger)
 
-	// Создаем мультиплексор маршрутов
 	mux := http.NewServeMux()
 
-	// Регистрируем обработчики эндпоинтов
 	mux.HandleFunc("/api/v1/calculate", server.HandleCalculate)
 	mux.HandleFunc("/api/v1/expressions", server.HandleGetExpressions)
 	mux.HandleFunc("/api/v1/expressions/", server.HandleGetExpressionByID)
 	mux.HandleFunc("/internal/task", server.HandleGetTask)
 	mux.HandleFunc("/internal/task/result", server.HandlePostTaskResult)
 
-	// Применяем middleware для логирования
 	loggedRouter := middleware.LoggingMiddleware(logger)(mux)
 
-	// Логируем инициализацию маршрутизатора
 	logger.Info("Router initialized with logging middleware")
 
 	return loggedRouter
