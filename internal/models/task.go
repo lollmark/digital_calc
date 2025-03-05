65bf255c8e7a22405b0ca8f4de03db70eb209c30
lollmark <markello2007@gmail.com>
2025-03-05 23:31:03 +0300

    Update task.go

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,34 +1,34 @@
 package models
 
-
 type Operation string
-
 
 const (
 	OperationAdd      Operation = "ADD"      
 	OperationSubtract Operation = "SUBTRACT" 
 	OperationMultiply Operation = "MULTIPLY" 
-	OperationDivide   Operation = "DIVIDE"   
+	OperationDivide   Operation = "DIVIDE"  
 )
 
 
 type Task struct {
-	ID           string    `json:"id"`                      
-	Expression   string    `json:"expression,omitempty"`    
-	Arg1         string    `json:"arg1,omitempty"`        
-	Arg2         string    `json:"arg2,omitempty"`          
-	Operation    Operation `json:"operation,omitempty"`    
+	ID           string    `json:"id"`                     
+	Expression   string    `json:"expression,omitempty"`  
+	Arg1         string    `json:"arg1,omitempty"`          
+	Arg2         string    `json:"arg2,omitempty"`         
+	Operation    Operation `json:"operation,omitempty"`
 	OperationTime int      `json:"operation_time,omitempty"`
-	Result       *float64  `json:"result,omitempty"`    
-	Dependencies []string  `json:"-"`                      
-	IsReady      bool      `json:"-"`                       
+	Result       *float64  `json:"result,omitempty"`        
+	Dependencies []string  `json:"-"`                       
+	IsReady      bool      `json:"-"`                      
 }
+
 
 type TaskResponse struct {
-	Task *Task `json:"task,omitempty"`
+	Task *Task `json:"task,omitempty"` 
 }
 
+
 type TaskResultRequest struct {
-	ID     string  `json:"id"`   
+	ID     string  `json:"id"`     
 	Result float64 `json:"result"` 
 }
