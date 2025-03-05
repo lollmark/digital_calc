19208e92fc40a6f6b11761b96e0434bd0e19afc6
lollmark <markello2007@gmail.com>
2025-03-05 23:11:06 +0300

    Update main.go

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"digitalcalc/internal/orchestrator"
-	"go.uber.org/zap"
+    "log"
+    "net/http"
+    "digitalcalc/internal/router"
+    "go.uber.org/zap"
 )
 
 func main() {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatal("Failed to initialize logger", err)
-	}
-	defer logger.Sync()
+    logger, err := zap.NewProduction()
+    if err != nil {
+        log.Fatal("Failed to initialize logger", err)
+    }
+    defer logger.Sync()
 
-	log.Println("Starting Orchestrator...")
-	router := orchestrator.NewRouter(logger)
-	log.Fatal(http.ListenAndServe(":8080", router))
+    log.Println("Starting Orchestrator...")
+    router := router.NewRouter(logger)
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
