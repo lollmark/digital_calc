3f3eed9b80bc677d0f95e560438e9442eddece1d
lollmark <markello2007@gmail.com>
2025-03-05 23:07:34 +0300

    Update expression.go

--- internal/models/expression.go
+++ internal/models/expression.go
@@ -1,30 +1,28 @@
 package models
 
-// Expression представляет арифметическое выражение и его статус
 type Expression struct {
-	ID       int     `json:"id"`                   // Уникальный идентификатор выражения
-	RawExpr  string  `json:"expression,omitempty"` // Исходное строковое выражение
-	Status   Status  `json:"status"`               // Текущий статус вычисления
-	Result   *string `json:"result,omitempty"`     // Результат вычисления (nil, если не вычислено)
-	ErrorMsg string  `json:"error,omitempty"`      // Сообщение об ошибке (если статус ERROR)
+	ID       string  `json:"id"`                  
+	RawExpr  string  `json:"expression,omitempty"` 
+	Status   Status  `json:"status"`               
+	Result   *string `json:"result,omitempty"`    
+	ErrorMsg string  `json:"error,omitempty"`      
 }
 
-// ExpressionRequest представляет запрос на вычисление выражения
 type ExpressionRequest struct {
-	Expression string `json:"expression"` // Строка с выражением
+	Expression string `json:"expression"` 
 }
 
-// ExpressionResponse представляет ответ на запрос добавления выражения
+
 type ExpressionResponse struct {
-	ID int `json:"id"` // Идентификатор добавленного выражения
+	ID string `json:"id"` 
 }
 
-// ExpressionsResponse представляет ответ со списком выражений
+
 type ExpressionsResponse struct {
-	Expressions []Expression `json:"expressions"` // Список выражений
+	Expressions []Expression `json:"expressions"` 
 }
 
-// ExpressionDetailResponse представляет ответ с детальной информацией о выражении
+
 type ExpressionDetailResponse struct {
-	Expression Expression `json:"expression"` // Детальная информация о выражении
+	Expression Expression `json:"expression"` 
 }
